e0a9a0c81654a39ee92648e042de86b24d0ce227
nikochiko <kaustubh299792458@gmail.com>
2021-02-05 13:23:26 +0530

    Fix stupid typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:github]
   
   def self.from_omniauth(auth)
-    where(provider: auth.proivider, uid: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
       user.email = auth.info.email
