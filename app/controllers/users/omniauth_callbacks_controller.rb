905128de9d3d037e42bb47102e26598bd4cbb279
nikochiko <kaustubh299792458@gmail.com>
2021-02-07 13:03:29 +0530

    Add github oauth connection to existing accounts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,21 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user
+    # Take omniauth info, if another user is already authenticated,
+    # try to add github omniauth on that user
+    # if not,
+    # and create an account with github-provided details
+    if user_signed_in?
+      @user = current_user
+      return render :html => 'A GitHub account is already connected' if @user.has_github_oauth
+
+      if @user.add_omniauth(request.env['omniauth.auth'])
+        render :html => 'GitHub account connected successfully!'
+      else
+        render :html => "Couldn't connect with GitHub account"
+      end
+    else
+      @user = User.from_omniauth(request.env['omniauth.auth'])
+      sign_in_and_redirect @user
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,48 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # TODO: Add and setup confirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
-  
+
+  # same github account shouldn't be connected twice
+  validates :uid, uniqueness: { scope: :provider }, if: -> { !provider.nil? }
+
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
+    if auth.provider == 'github'
+      # if provider-uid exists return that user
+      # else see if the primary_email is associated with another user
+      # if yes, return that user (without adding provider-uid)
+      # else create a new user with the fetched details
+
+      # fetch and return user with provided github details if present
+      user = where(provider: auth.provider, uid: auth.uid).first
+      return user if user
+
+      # return user with that email id or create a new user
+      where(email: auth.info.email).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.password = Devise.friendly_token[0, 20]
+        # user.skip_confirmation!
+      end
+    end
+  end
+
+  def has_github_oauth
+    provider == 'github'
+  end
+
+  def add_omniauth(auth)
+    if auth.provider == 'github'
+      # Add omniauth details to user unless the provider account is already
+      # connected to another account
+      unless self.class.where(provider: auth.provider, uid: auth.uid).exists?
+        self.provider = auth.provider
+        self.uid = auth.uid
+        self.save
+      end
     end
   end
 end
