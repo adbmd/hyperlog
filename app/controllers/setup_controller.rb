c852016867e33c702e222515ed5404d40e34d65e
Aditya Giri <dtrg21@gmail.com>
2021-04-03 13:18:23 +0530

    Change from concat to interpolation

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -3,7 +3,7 @@ class SetupController < ApplicationController
 
   def index
     if current_user.setup_step.positive?
-      redirect_to('/setup/' + current_user.setup_step.to_s)
+      redirect_to("/setup/#{current_user.setup_step}")
     else
       redirect_to '/', alert: 'Setup Already Completed'
     end
@@ -11,44 +11,44 @@ class SetupController < ApplicationController
 
   def step_one
     if current_user.setup_step != 1
-      redirect_to('/setup/' + current_user.setup_step.to_s)
+      redirect_to("/setup/#{current_user.setup_step}")
     end
   end
 
   def step_two
     if current_user.setup_step != 2
-      redirect_to('/setup/' + current_user.setup_step.to_s)
+      redirect_to("/setup/#{current_user.setup_step}")
     end
   end
 
   def step_three
     if current_user.setup_step != 3
-      redirect_to('/setup/' + current_user.setup_step.to_s)
+      redirect_to("/setup/#{current_user.setup_step}")
     end
   end
 
   def step_four
     if current_user.setup_step != 4
-      redirect_to('/setup/' + current_user.setup_step.to_s)
+      redirect_to("/setup/#{current_user.setup_step}")
     end
   end
 
   def step_one_submit
     current_user.setup_step = 2
     current_user.save
-    redirect_to('/setup/' + current_user.setup_step.to_s)
+    redirect_to("/setup/#{current_user.setup_step}")
   end
 
   def step_two_submit
     current_user.setup_step = 3
     current_user.save
-    redirect_to('/setup/' + current_user.setup_step.to_s)
+    redirect_to("/setup/#{current_user.setup_step}")
   end
 
   def step_three_submit
     current_user.setup_step = 4
     current_user.save
-    redirect_to('/setup/' + current_user.setup_step.to_s)
+    redirect_to("/setup/#{current_user.setup_step}")
   end
 
   def step_four_submit
@@ -60,6 +60,6 @@ class SetupController < ApplicationController
   def previous_step
     current_user.setup_step -= 1
     current_user.save
-    redirect_to('/setup/' + current_user.setup_step.to_s)
+    redirect_to("/setup/#{current_user.setup_step}")
   end
 end
