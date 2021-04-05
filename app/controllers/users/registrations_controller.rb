780be167197dff2e343f1cb551d7d91627ff27f9
Aditya Giri <dtrg21@gmail.com>
2021-04-05 14:58:45 +0530

    Fix user redirecting to home after signup

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_update_path_for(_resource)
-    login_path
+    setup_path
   end
 
   def after_sign_up_path_for(_resource)
