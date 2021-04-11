9fee50320f313897a875ac1f8626f01417db4af2
Aditya Giri <dtrg21@gmail.com>
2021-04-11 15:09:12 +0530

    Use theme image

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Theme.create(name: 'spectre')
+Theme.create(name: 'papermod')
 
 user = User.new(
   email: 'test@test.com',
